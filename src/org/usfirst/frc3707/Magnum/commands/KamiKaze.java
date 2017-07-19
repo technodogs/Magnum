3cecd29762de69b0946ce7acf3fd8feda94cc319
angiedelisle <a2deli19@gmail.com>
2017-07-19 18:38:03 -0400

    xboxDrive and KamiKaze button

--- src/org/usfirst/frc3707/Magnum/commands/KamiKaze.java
+++ src/org/usfirst/frc3707/Magnum/commands/KamiKaze.java
@@ -41,7 +41,7 @@ public class KamiKaze extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.drivesystem.drive(1,1);
+    	Robot.drivesystem.tankdrive(0.7,0.7);
     }
 
     // Make this return true when this Command no longer needs to run execute()
